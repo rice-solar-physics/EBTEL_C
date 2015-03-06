c896ff1dff54fc0383a25d27d50159335bb5f16e
Will Barnes <wtb2@rice.edu>
2015-03-05 19:15:07 -0600

    Added option in EBTEL wrapper function to allow for non-interactive plotting and printing to file

--- bin/ebtel_wrapper.py
+++ bin/ebtel_wrapper.py
@@ -8,6 +8,8 @@
 import os,os.path
 import subprocess
 import numpy as np
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
 def plot_ebtel_dem(data_directory,data_file,**kwargs):
